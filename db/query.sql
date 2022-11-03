856d4588ac16b8ba8a4eb676bd823db8ba72f3fd
bryangholipour <113397778+bryangholipour@users.noreply.github.com>
2022-11-02 20:14:07 -0700

    tweaks

--- db/query.sql
+++ db/query.sql
@@ -2,10 +2,14 @@
 --JOIN table
 --ON table.id (pets.owner_id=owners.id)
 
+
+
 -- SELECT departments.department_name AS Departments, roles.title AS Title
 -- FROM roles
 -- JOIN employees
 -- ON employees.id = departments.id;
+
+
 
 -- SELECT departments.department_name AS department, reviews.review
 -- FROM reviews
